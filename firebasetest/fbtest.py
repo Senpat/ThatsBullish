95ed85c2138f94ed12f8e66fd228cdfbab0f0846
Senpat <pgz11902@gmail.com>
2021-02-28 03:02:59 -0500

    python firebase

---
+++ firebasetest/fbtest.py
@@ -0,0 +1,16 @@
+import firebase_admin
+
+from firebase_admin import db
+
+import json
+
+cred_obj = firebase_admin.credentials.Certificate('thatsbullish-15d47-firebase-adminsdk-a0q0p-3afad4f863.json')
+default_app = firebase_admin.initialize_app(cred_obj,{
+    'databaseURL':'https://thatsbullish-15d47-default-rtdb.firebaseio.com'
+})
+
+ref = db.reference("stocks")
+
+with open("addtest.json","r") as f:
+    addtest = json.load(f)
+ref.set(addtest)
